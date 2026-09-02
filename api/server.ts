ce42a84bf241b51dbff44e06afdb141db4836cef
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-09-02 09:43:37 +0100

    perf: ajuste no caminho da routa

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { userRoutes } from "./routes/user.route"; // ajuste o caminho
+import { userRoutes } from "./routes/user.route"; 
 
 const app = express();
 
 app.use(express.json());
-app.use(userRoutes);
+app.use("/api", userRoutes);
 
 app.listen(3000, () => {
     console.log("Servidor a rodar na porta 3000");
