18a08d7214d0cae036ab579766f77b7c85584820
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-09-02 09:27:55 +0100

    feat: instalação pinia e toast

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import ToastService from 'primevue/toastservice';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import 'primeicons/primeicons.css';
@@ -10,6 +12,8 @@ import router from './router';
 const app = createApp(App);
 
 app.use(router);
+app.use(createPinia());
+app.use(ToastService);
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
