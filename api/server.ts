bbaddd9dc96aa501844899a8924414e573cb6418
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-09-02 09:49:23 +0100

    feat: instalação do cors

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { userRoutes } from "./routes/user.route"; 
-
+import cors from "cors";
 const app = express();
+
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 app.use("/api", userRoutes);
