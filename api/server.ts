a73b8cfbf359d507b7aa4f06aff88c207f854d7d
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-09-05 15:53:22 +0100

    perf: rota de teste

--- api/server.ts
+++ api/server.ts
@@ -18,6 +18,9 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", consumptionPlanRoutes);
 app.use("/api", consumptionRoutes);
+app.get('/api', (req, res) => {
+  res.json({ message: 'API rodando...!' });
+});
 
 app.listen(3000, () => {
     console.log("Servidor a rodar na porta 3000");
