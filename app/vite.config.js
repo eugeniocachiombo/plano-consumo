f3c41001b67219014ce6da3d81cc64293189f87a
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-09-05 12:23:37 +0100

    feat: pwa instalação

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'PlanoK - Gestão de Consumo',
+        short_name: 'VuePWA',
+        description: 'Descrição do meu aplicativo Vue PWA',
+        theme_color: '#ffffff',
+        devOptions: {
+          enabled: true 
+        },
+        icons: [
+          {
+            src: '/assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/img/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5173,
     host: true
@@ -12,5 +39,6 @@ export default defineConfig({
     alias: {
       '@': '/src'
     }
-  }
+  },
+  
 });
