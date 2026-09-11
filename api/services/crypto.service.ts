81e8200383bb6e3ef285b72b85f5d360557a74e9
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-09-11 09:27:35 +0100

    test

--- api/services/crypto.service.ts
+++ api/services/crypto.service.ts
@@ -1,7 +1,7 @@
 import CryptoJS from 'crypto-js';
 const CRYPTO_SECRET_KEY = String('secret');
 // const CRYPTO_SECRET_KEY = String(process.env.CRYPTO_SECRET_KEY);
-console.log( String(process.env.CRYPTO_SECRET_KEY))
+console.log("api", String(process.env.CRYPTO_SECRET_KEY))
 
 class CryptoService {
     public encryptId(text: string) {
