ef3da96fe9587da8dcffa6e3fdee30fc16133ba1
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-09-11 09:44:59 +0100

    perf: crypt sobre String()

--- app/src/services/crypto.service.ts
+++ app/src/services/crypto.service.ts
@@ -1,5 +1,5 @@
 import CryptoJS from 'crypto-js';
-const CRYPTO_SECRET_KEY = import.meta.env.VITE_CRYPTO_SECRET_KEY
+const CRYPTO_SECRET_KEY = String(import.meta.env.VITE_CRYPTO_SECRET_KEY)
 
 class CryptoService {
     public encryptId(text: string) {
